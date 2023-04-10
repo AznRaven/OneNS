7f951229caaeb5545158744aa2d80adb42e8a401
AznRaven <versatilias@gmail.com>
2023-04-10 19:15:40 -0400

    Add Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+export default function Footer() {
+    const currentYear = new Date().getFullYear();
+
+  return (
+    <>
+      <footer class="py-4 bg-light mt-auto">
+        <div class="container-fluid px-4">
+          <div class="d-flex align-items-center justify-content-between small">
+            <div class="text-muted">Copyright &copy; 1NailSystem {currentYear}</div>
+            <div>
+              <a href="#">Privacy Policy</a>
+              &middot;
+              <a href="#">Terms &amp; Conditions</a>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+}
