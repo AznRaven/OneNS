2e37714d61fdb051063f532b0a37a3452b92554d
AznRaven <versatilias@gmail.com>
2023-04-11 16:04:16 -0400

    fixed alignment addChair

--- client/src/pages/ChairRecords/New.js
+++ client/src/pages/ChairRecords/New.js
@@ -13,7 +13,7 @@ export default function New() {
           <div className="row techChair">
             {/* Technician */}
             <div className="col-xl-8 d-flex">
-              <div className="card shadow mb-2 flex-fill">
+              <div className="card shadow flex-fill">
                 <div className="card-header">
                   <i className="fas fa-user me-1"></i>
                   Technician
@@ -30,52 +30,87 @@ export default function New() {
                 </div>
                 <div className="card-body">
                   <div class="row">
-                    <button type="button" class="btn btn-outline-secondary chairBtn col">
+                    <button
+                      type="button"
+                      class="btn btn-outline-secondary chairBtn col"
+                    >
                       7
                     </button>
-                    <button type="button" class="btn btn-outline-secondary chairBtn col">
+                    <button
+                      type="button"
+                      class="btn btn-outline-secondary chairBtn col"
+                    >
                       8
                     </button>
-                    <button type="button" class="btn btn-outline-secondary chairBtn col">
+                    <button
+                      type="button"
+                      class="btn btn-outline-secondary chairBtn col"
+                    >
                       9
                     </button>
                   </div>
                   <div class="row">
-                    <button type="button" class="btn btn-outline-secondary chairBtn col">
+                    <button
+                      type="button"
+                      class="btn btn-outline-secondary chairBtn col"
+                    >
                       4
                     </button>
-                    <button type="button" class="btn btn-outline-secondary chairBtn col">
+                    <button
+                      type="button"
+                      class="btn btn-outline-secondary chairBtn col"
+                    >
                       5
                     </button>
-                    <button type="button" class="btn btn-outline-secondary chairBtn col">
+                    <button
+                      type="button"
+                      class="btn btn-outline-secondary chairBtn col"
+                    >
                       6
                     </button>
                   </div>
                   <div class="row">
-                    <button type="button" class="btn btn-outline-secondary chairBtn col">
+                    <button
+                      type="button"
+                      class="btn btn-outline-secondary chairBtn col"
+                    >
                       1
                     </button>
-                    <button type="button" class="btn btn-outline-secondary chairBtn col">
+                    <button
+                      type="button"
+                      class="btn btn-outline-secondary chairBtn col"
+                    >
                       2
                     </button>
-                    <button type="button" class="btn btn-outline-secondary chairBtn col">
+                    <button
+                      type="button"
+                      class="btn btn-outline-secondary chairBtn col"
+                    >
                       3
                     </button>
                   </div>
                   <div class="row">
-                    <button type="button" class="btn btn-outline-primary chairBtn col">
+                    <button
+                      type="button"
+                      class="btn btn-outline-primary chairBtn col"
+                    >
                       Clear
                     </button>
-                    <button type="button" class="btn btn-outline-secondary chairBtn col">
+                    <button
+                      type="button"
+                      class="btn btn-outline-secondary chairBtn col"
+                    >
                       0
                     </button>
-                    <button type="button" class="btn btn-outline-danger chairBtn col">
+                    <button
+                      type="button"
+                      class="btn btn-outline-danger chairBtn col"
+                    >
                       Submit
                     </button>
                   </div>
                 </div>
               </div>
-
 
               <div className="card shadow flex-fill">
                 <div className="card-header">
@@ -84,7 +119,6 @@ export default function New() {
                 </div>
                 <div className="card-body"></div>
               </div>
-              
             </div>
             {/* Details */}
             <div className="col-xl-4"></div>
