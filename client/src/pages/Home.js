db629bf410a877a33b81c67859e2013992cbbfbb
AznRaven <versatilias@gmail.com>
2023-04-13 02:30:05 -0400

    fix alignment

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,7 +2,7 @@ import Sidenav from "../components/Sidenav";
 import Login from "./Login";
 
 export default function Home() {
-  let user = false
+  let user = true
   return (
     <>
       {user ? <Sidenav/> : <Login/>}
