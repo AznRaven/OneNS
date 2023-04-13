3d625482818602369c84bbd1623e7681b947239e
AznRaven <versatilias@gmail.com>
2023-04-12 20:03:56 -0400

    login = false

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,7 +2,7 @@ import Sidenav from "../components/Sidenav";
 import Login from "./Login";
 
 export default function Home() {
-  let user = true
+  let user = false
   return (
     <>
       {user ? <Sidenav/> : <Login/>}
