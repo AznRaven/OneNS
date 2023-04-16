a6d971492733a23d3a8259eebd24ef69c49b6202
AznRaven <versatilias@gmail.com>
2023-04-15 21:31:55 -0400

    remove demo files

--- client/src/components/Sidenav.js
+++ client/src/components/Sidenav.js
@@ -18,33 +18,6 @@ export default function Sidenav(props) {
                   Dashboard
                 </a> */}
                 <div class="sb-sidenav-menu-heading">Interface</div>
-                <div class="dropdown">
-                  <button
-                    class="btn btn-light dropdown-toggle"
-                    type="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Dropdown button
-                  </button>
-                  <ul class="dropdown-menu">
-                    <li>
-                      <a class="dropdown-item" href="#">
-                        Action
-                      </a>
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="#">
-                        Another action
-                      </a>
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="#">
-                        Something else here
-                      </a>
-                    </li>
-                  </ul>
-                </div>
                 <div
                   class="collapse"
                   id="collapseLayouts"
