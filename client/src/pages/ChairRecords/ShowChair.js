9967de3c4a548901e0791d25aa0af7e926e683e0
AznRaven <versatilias@gmail.com>
2023-04-16 22:31:25 -0400

    show chair table

--- client/src/pages/ChairRecords/ShowChair.js
+++ client/src/pages/ChairRecords/ShowChair.js
@@ -11,40 +11,77 @@ export default function ShowChair() {
         <div id="layoutSidenav_content">
           <div className="bg-secondary showChairBg d-flex justify-content-center align-items-center">
             <table
-              class="showChairTable table bg-light text-center"
+              class="showChairTable table bg-light"
               style={{ width: "90%" }}
             >
-              <thead className="table-borderless">
+              <thead>
                 <tr>
-                  <th colspan="12" scope="col" >TEXAS DEPARTMENT OF LICENSING & REGULATIONS </th>
+                  <th className="border-light text-center" colspan="12" >TEXAS DEPARTMENT OF LICENSING & REGULATIONS </th>
                 </tr>
                 <tr>
-                  <td colspan="12">TEXAS DEPARTMENT OF LICENSING & REGULATIONS</td>
+                  <td className="border-light text-center" colspan="12">P.O. Box 12157 Austin, Texas 78711-2157 Fax(512) 583-7148</td>
                 </tr>
                 <tr>
-                  <th scope="col">#</th>
-                  <th scope="col">First</th>
-                  <th scope="col">Last</th>
-                  <th scope="col">Handle</th>
+                  <td className="border-light text-center" colspan="12">inspectionrequest@tdlr.texas.gov www.tdlr.texas.gov</td>
+                </tr>
+                <tr>
+                  <td className="border-light text-center" colspan="12">BARBERING & COSMETOLOGY PROGRAM</td>
+                </tr>
+                <tr>
+                  <td className="border-light text-center bg-black text-white" colspan="12">WHIRLPOOL FOOT SPA CLEANING AND DISINFECTION RECORD (TAC 83.108)</td>
+                </tr>
+                <tr>
+                  <td className="border" colspan="2">Name:</td>
+                  <td className="border" colspan="1"></td>
+                  <td className="border" colspan="3">License Number:</td>
+                  <td className="border" colspan="6"></td>
+                </tr>
+                <tr>
+                  <td className="border" colspan="2">Address:</td>
+                  <td className="border" colspan="1"></td>
+                  <td className="border" colspan="3">Basin/Tub Number:</td>
+                  <td className="border" colspan="6"></td>                
+                </tr>
+                <tr>
+                  <td colspan="3"></td>
+                  <td colspan="3"></td>
+                  <td colspan="3"></td>
+                  <td colspan="3"></td>
                 </tr>
               </thead>
-              <tbody>
+              <tbody className="text-center">
                 <tr>
-                  <th scope="row">1</th>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                  <td>@mdo</td>
+                    <th className="border" colspan="2">When</th>
+                    <th className="border" colspan="1">Who</th>
+                    <th className="border" colspan="9">CLEANED and DISINFECTED<br/><div className="">Place X in Correct Box or Boxes</div></th>
+                </tr>
+                
+                <tr style={{fontSize: "1.9vh"}}>
+                    <th className="border" style={{width: "10%"}} class="name">Date<br/>MM/DD/YYYY</th>
+                    <th className="border" style={{width: "10%"}}>Time<br/>A.M. or P.M.</th>
+                    <th className="border" style={{width: "33%"}}>Full Name</th>
+                    <th className="border" style={{width: "5%"}}>Chair Number</th>
+                    <th className="border" style={{width: "5%"}}>Not used</th>
+                    <th className="border" style={{width: "10%"}}>Liner used and disposed</th>
+                    <th className="border" style={{width: "10%"}}>Portable Jet Used and Cleaned</th>
+                    <th className="border" style={{width: "10%"}}>After Client</th>
+                    <th className="border" style={{width: "10%"}}>End of Day</th>
+                    <th className="border" style={{width: "10%"}}>Bi-Weekly</th>
+                    <th className="border" style={{width: "10%"}} class="noprint" colspan="2">Edit</th>
                 </tr>
                 <tr>
-                  <th scope="row">2</th>
-                  <td>Jacob</td>
-                  <td>Thornton</td>
-                  <td>@fat</td>
-                </tr>
-                <tr>
-                  <th scope="row">3</th>
-                  <td colspan="2">Larry the Bird</td>
-                  <td>@twitter</td>
+                  <td>1</td>
+                  <td>1</td>
+                  <td>1</td>
+                  <td>1</td>
+                  <td>1</td>
+                  <td>1</td>
+                  <td>1</td>
+                  <td>1</td>
+                  <td>1</td>
+                  <td>1</td>
+                  <td>1</td>
+                  <td>1</td>
                 </tr>
               </tbody>
             </table>
--- client/src/pages/Testing.js
+++ client/src/pages/Testing.js
@@ -1,50 +1,8 @@
-import { useState } from "react";
-
 export default function Testing() {
-  const [datetime, setDatetime] = useState(
-    new Date()
-      .toLocaleString([], {
-        year: "numeric",
-        month: "2-digit",
-        day: "2-digit",
-        hour: "2-digit",
-        minute: "2-digit",
-      })
-      .replace(",", "")
-  );
-  console.log(datetime);
-  const subtractHours = (date, hours) => {
-    return new Date(date.getTime() - hours * 60 * 60 * 1000);
-  };
 
-  const handleDatetimeChange = (e) => {
-    setDatetime(e.target.value);
-  };
+  return(
+  <>
 
-  const datetimeMinus4Hours = subtractHours(new Date(datetime), 4);
-
-  const formattedDatetime = datetimeMinus4Hours.toISOString().substr(0, 16);
-
-  return (
-    <>
-      <div className="card mb-4">
-        <div className="card-header">
-          <i className="fas fa-table me-1"></i>
-          Cleaned & Disinfected
-        </div>
-        <div className="card-body">
-          <div className="row d-flex ">
-            <div className="form-floating mb-3 col-12 text-center">
-              <input
-                className="rounded form-control fs-6"
-                type="datetime-local"
-                value={formattedDatetime.replace(" ", "T")}
-                onChange={handleDatetimeChange}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+  </>
+  )
 }
